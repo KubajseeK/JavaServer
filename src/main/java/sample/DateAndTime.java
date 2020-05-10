8ac943c99eb95cc5c2e683945efe558c4d48221e
Jakub Kutka <jacobkutka@gmail.com>
2020-05-10 16:55:17 +0200

    10.05.2020

--- src/main/java/sample/DateAndTime.java
+++ src/main/java/sample/DateAndTime.java
@@ -6,11 +6,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.SecureRandom;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 public class DateAndTime {
@@ -24,20 +24,21 @@ public class DateAndTime {
 
     @RequestMapping("/time")
     public ResponseEntity<String> getTime(@RequestParam(value = "token") String token) {
-        JSONObject jsonObject = new JSONObject();
 
         if (token == null) {
             return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body("{\"error\",\"No token found.\"}");
         }
 
-        if (findToken(token)) {
-            LocalTime time = LocalTime.now();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-            jsonObject.put("time", formatter);
+        if (isTokenValid(token)) {
+            JSONObject jsonObject = new JSONObject();
+            SimpleDateFormat sdfDate = new SimpleDateFormat("HH:mm:ss");
+            Date now = new Date();
+            String strTime = sdfDate.format(now);
+            jsonObject.put("time",strTime);
             return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(jsonObject.toString());
         } else {
-            jsonObject.put("error", "Invalid token");
-            return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body(jsonObject.toString());
+
+            return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body("{\"error\":\"Invalid token\"}");
         }
     }
 
@@ -65,12 +66,10 @@ public class DateAndTime {
 
     @RequestMapping("/time/hour")
     public ResponseEntity<String> getHour() {
-        JSONObject jsonObject = new JSONObject();
-
-        LocalTime time = LocalTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH");
-        jsonObject.put("hour", formatter);
-        return ResponseEntity.status(200).body(jsonObject.toString());
+        SimpleDateFormat sdfDate = new SimpleDateFormat("HH");
+        Date now = new Date();
+        String strHour = sdfDate.format(now);
+        return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body("{\"hour\":"+strHour+"}");
     }
 
     @RequestMapping("/hello")
@@ -106,10 +105,10 @@ public class DateAndTime {
                     return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body("{}");
                 }
 
-                String token = Long.toString(Math.abs(new SecureRandom().nextLong()), 16);
                 response.put("fname", templateUser.getFname());
                 response.put("lname", templateUser.getLname());
                 response.put("login", templateUser.getLogin());
+                String token = generateToken();
                 response.put("token", token);
                 templateUser.setToken(token);
                 return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(response.toString());
@@ -120,9 +119,9 @@ public class DateAndTime {
             }
 
         } else {
-            JSONObject res = new JSONObject();
-            res.put("error", "Invalid body request");
-            return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body(res.toString());
+            JSONObject response = new JSONObject();
+            response.put("error", "Invalid body request");
+            return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body(response.toString());
         }
     }
 
@@ -136,7 +135,7 @@ public class DateAndTime {
             return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body("{\"error\",\"Bad request\"}");
         }
 
-        if (findToken(token)) {
+        if (isTokenValid(token)) {
 
             if (user == null) {
                 return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body("{\"error\":\"Invalid User\"}");
@@ -154,7 +153,6 @@ public class DateAndTime {
     @RequestMapping(method = RequestMethod.POST, value = "/signup")
     public ResponseEntity<String> signup(@RequestBody String data) {
         JSONObject jsonObject = new JSONObject(data);
-        String password = BCrypt.hashpw(jsonObject.getString("password"), BCrypt.gensalt(6));
 
         if (jsonObject.has("fname") && jsonObject.has("lname") && jsonObject.has("login") && jsonObject.has("password")) {
             if (findLogin(jsonObject.getString("login"))) {
@@ -162,12 +160,14 @@ public class DateAndTime {
                 response.put("error", "User already exists");
                 return ResponseEntity.status(400).body(response.toString());
             }
+            String password = jsonObject.getString("password");
             if (password.isEmpty()) {
                 JSONObject response = new JSONObject();
                 response.put("error", "Password field can not be empty");
                 return ResponseEntity.status(400).body(response.toString());
             }
-            User user = new User(jsonObject.getString("fname"), jsonObject.getString("lname"), jsonObject.getString("login"), jsonObject.getString("password"));
+            String hashPass = hash(jsonObject.getString("password"));
+            User user = new User(jsonObject.getString("fname"), jsonObject.getString("lname"), jsonObject.getString("login"), hashPass);
             userList.add(user);
 
             JSONObject response = new JSONObject();
@@ -190,7 +190,7 @@ public class DateAndTime {
         String login = jsonObject.getString("login");
         User user = getUser(login);
 
-        if (user != null && findToken(token)) {
+        if (user != null && isTokenValid(token)) {
             user.setToken(null);
             return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body("{}");
         }
@@ -241,6 +241,35 @@ public class DateAndTime {
         return false;
     }
 
+    private String generateToken() {
+        int size=25;
+        Random rnd = new Random();
+        String generatedString="";
+        for(int i = 0;i<size;i++) {
+            int type=rnd.nextInt(4);
+
+            switch (type) {
+                case 0:
+                    generatedString += (char) ((rnd.nextInt(26)) + 65);
+                    break;
+                case 1:
+                    generatedString += (char) ((rnd.nextInt(10)) + 48);
+                    break;
+                default:
+                    generatedString += (char) ((rnd.nextInt(26)) + 97);
+            }
+        }
+        return generatedString;
+    }
+
+    public boolean isTokenValid(String token) {
+        for(User useri : userList)
+            if(useri.getToken()!=null && useri.getToken().equals(token))
+                return true;
+
+        return false;
+    }
+
     private boolean compareLogin(String login, String password) {
         for (User user : userList) {
             if (user.getLogin().equals(login) && user.getPassword().equals(password)) {
@@ -248,6 +277,10 @@ public class DateAndTime {
             }
         }
         return true;
+    }
+
+    public String hash(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt(12));
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/log?type={logType}")
@@ -302,14 +335,15 @@ public class DateAndTime {
                 response.put("error", "Invalid Token");
                 return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body(response.toString());
             }
-            if (compareLogin(user.getString("login"), user.getString("oldpassword"))) {
+            if (!findLogin(user.getString("login")) || !checkPassword(user.getString("login"), user.getString("oldpassword"))) {
                 response.put("error", "Incorrect login or Password");
                 return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body(response.toString());
             }
 
+            String hashPass = hash(user.getString("newpassword"));
             for (User users : userList) {
                 if (users.getLogin().equals(user.getString("login"))) {
-                    users.setPassword(user.getString("newpassword"));
+                    users.setPassword(hashPass);
                     response.put(users.getLogin(), "password changed");
                     return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(response.toString());
                 }
